e8fd638db55b1b7767091a2b962721de7ae55781
Chris <christopher@chooo.media>
2020-05-13 04:41:46 +0200

    some changes

--- 404.php
+++ 404.php
@@ -9,14 +9,14 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area mt-5 pt-5 container">
-		<main id="main" class="site-main d-flex justify-center error-404" role="main" style="background: url()">
+	<section id="primary" class="content-area mt-5 pt-5 px-0 container">
+		<main id="main" class="site-main d-flex justify-center error-404" role="main" style="background: url(<?php echo get_template_directory_uri() ;?>/inc/assets/img/404-background-Paper-Tear.png);">
 
-		<section class="not-found text-center col-12 my-auto">
+		<section class="not-found text-center col-12 my-auto text-white">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-times-art' ); ?></h1>
+					<h1 class="page-title text-white"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-times-art' ); ?></h1>
 				</header><!-- .page-header -->
-
+				
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-times-art' ); ?></p>
 
