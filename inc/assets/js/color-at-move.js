0c454d32cabc9f7742809a295695ecabb3ae26f1
chooomedia <christopher@chooomedia.de>
2020-07-14 12:54:22 +0200

    changed to default

--- inc/assets/js/color-at-move.js
+++ inc/assets/js/color-at-move.js
@@ -1,30 +1,29 @@
 jQuery(function($) {
-    let portfolioBox = $(".post-img");
-    let headerThumb = $(".header-thumbnail");
-    let blockImg = $(".wp-block-image img");
-    let page = $("#page");
-    let isMousemove = false;
+    var canvasWrapper = $("#canvasWrapper");
+    var portfolioBox = $(".post-img");
+    var headerThumb = $(".header-thumbnail");
+    var blockImg = $(".wp-block-image img");
+
+    var page = $("#page");
+
+    var isMousemove = false;
 
     page.on("mousemove", function (event) {
         isMousemove = true;
     });
 
     page.mousestop(function() {
-        if($("iframe").length > 0) {
-            let iframe = $("iframe");
-            iframe.addClass("no-color");
-        }     
+        canvasWrapper.addClass("no-color");
         portfolioBox.addClass("no-color");
+        headerThumb.addClass("no-color");
         blockImg.addClass("no-color");
     });
 
     page.on("mousemove", function (event) {
         if (isMousemove) {
-            if($("iframe").length > 0) {
-                iframe = $("iframe");
-                iframe.removeClass("no-color");
-            } 
+            canvasWrapper.removeClass("no-color");
             portfolioBox.removeClass("no-color");
+            headerThumb.removeClass("no-color");
             blockImg.removeClass("no-color");
         }
     });
