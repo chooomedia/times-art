a029e1d41799fc62430d588f262e19ffa1140acc
chooomedia <christopher@chooomedia.de>
2020-07-14 23:46:18 +0200

    renaming footer widget id

--- footer-widget.php
+++ footer-widget.php
@@ -13,8 +13,8 @@ if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_ac
                     <?php if ( is_active_sidebar( 'footer-3' )) : ?>
                         <div class="col-12 col-md-3 p-0 m-0 d-flex align-items-center"><?php dynamic_sidebar( 'footer-3' ); ?></div>
                     <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-                        <div class="col-12 col-md-1 p-0 m-0 d-flex justify-content-md-end justify-content-center my-4"><?php dynamic_sidebar( 'footer-4' ); ?></div>
+                    <?php if ( is_active_sidebar( 'footer-4' )) : ?>
+                        <div class="col-12 col-md-1 p-0 m-0 d-flex justify-content-md-end justify-content-center mt-4"><?php dynamic_sidebar( 'footer-4' ); ?></div>
                     <?php endif; ?>
                 </div>
             </div>
