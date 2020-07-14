29f152003e64b296d321ef4d45d90e534b1947b5
chooomedia <christopher@chooomedia.de>
2020-07-14 13:02:33 +0200

    changes to org single.php

--- single.php
+++ single.php
@@ -11,7 +11,6 @@ get_header(); ?>
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<img class="thumbs" />
 
 		<?php
 		while ( have_posts() ) : the_post();
@@ -30,24 +29,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
-	<script>
-	jQuery(function($) {
-		if($('iframe').length > 0) {
-			let vimeoIframeUrl = $('iframe').attr('src');
-			let video_id = vimeoIframeUrl.split('video/')[1];
-			let ampersandPosition = video_id.indexOf('?dnt');
-
-			if(ampersandPosition != -1) {
-				video_id = video_id.substring(0, ampersandPosition);
-			}
-
-			$.getJSON('https://www.vimeo.com/api/v2/video/' + video_id + '.json?callback=?', {format: "json"}, function(data) {
-				$('#masthead').css('background-image', 'url('+ data[0].thumbnail_large +')');
-				console.log(data);
-			});
-		}
-	});
-	</script>
 
 <?php
 get_footer();
